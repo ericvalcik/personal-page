107713b5c284e18b28d93023124724128b3e56eb
eric valcik <eric.valcik@gmail.com>
2023-03-25 14:49:03 +0100

    Dynamic animation to routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,22 @@
+"use client";
+
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import clsx from "clsx";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+    setTimeout(() => router.push("/portfolio"), 1000);
+  };
+
   return (
     <main className={styles.main}>
-      <div className={clsx(styles.description, "invisible")}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
       <div className={styles.center}>
         <span
           className={clsx(
@@ -41,51 +26,24 @@ export default function Home() {
         >
           Eric Valcik
         </span>
-        <div className={styles.thirteen}>
-          <span className="text-3xl">😜</span>
+        <div
+          className={clsx(
+            styles.thirteen,
+            "left-[calc(50%+70px)]",
+            isClicked && "h-[120vh] w-[120vw] !-left-[70px]"
+          )}
+          onClick={handleClick}
+        >
+          <span className="text-3xl">
+            <Image
+              src="/magnifying-glass-solid.svg"
+              alt="Magnifying glass icon"
+              width={24}
+              height={24}
+              priority
+            />
+          </span>
         </div>
-      </div>
-
-      <div className={clsx(styles.grid, "invisible")}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
