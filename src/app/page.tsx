24635996791c5e6c3bcbe758451ac97f41554129
eric valcik <eric.valcik@gmail.com>
2023-04-24 18:32:59 +0100

    simplify the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,45 @@
-"use client";
-
-import Image from "next/image";
-import styles from "./page.module.css";
-import clsx from "clsx";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-
 export default function Home() {
-  const router = useRouter();
-  const [isClicked, setIsClicked] = useState(false);
-
-  const handleClick = () => {
-    setIsClicked(!isClicked);
-    setTimeout(() => router.push("/portfolio"), 1000);
-  };
-
   return (
-    <main className={styles.main}>
-      <div className={styles.center}>
-        <span
-          className={clsx(
-            styles.text,
-            "text-4xl font-semibold tracking-tighter pr-1"
-          )}
-        >
-          Eric Valcik
-        </span>
-        <div
-          className={clsx(
-            styles.thirteen,
-            "left-[calc(50%+70px)]",
-            isClicked && "h-[120vh] w-[120vw] !-left-[70px]"
-          )}
-          onClick={handleClick}
-        >
-          <span className="text-3xl">
-            <Image
-              src="/magnifying-glass-solid.svg"
-              alt="Magnifying glass icon"
-              width={24}
-              height={24}
-              priority
-            />
-          </span>
+    <div className="h-screen w-screen flex items-start justify-center sm:pt-40 pt-8 text-sm sm:text-base">
+      <div className="w-[640px] flex flex-col justify-center items-start sm:px-0 px-4">
+        <h1 className="text-[21.2px] sm:text-2xl pb-16 sm:pb-20 font-semibold">
+          About
+        </h1>
+        <div>
+          I&apos;m a developer based in the Czech Republic. I&apos;m interested
+          in a wide range of topics, including web development, design and
+          discovering new ways to use technology.
+        </div>
+        <div className="pt-6">
+          I started coding in high school and got my first job as a developer in
+          the first year of my bachelor&apos;s studies. I got my bachelor&apos;s
+          degree in Computer Science from Masaryk University.
+        </div>
+        <hr className="my-12 w-full" />
+        <div className="flex gap-4">
+          <a
+            className="underline decoration-gray-200 hover:decoration-gray-600 transition-all duration-200"
+            href="#"
+          >
+            Twitter
+          </a>
+          <a
+            className="underline decoration-gray-200 hover:decoration-gray-600 transition-all duration-200"
+            href="#"
+          >
+            GitHub
+          </a>
+          <a
+            className="underline decoration-gray-200 hover:decoration-gray-600 transition-all duration-200"
+            href="#"
+          >
+            Instagram
+          </a>
+        </div>
+        <div className="pt-20 text-gray-400 text-[11.2px] sm:text-xs">
+          {`${new Date().getFullYear()}`} © Eric Valcik
         </div>
       </div>
-    </main>
+    </div>
   );
 }
--- src/app/portfolio/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Portfolio() {
-  return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      This is portfolio
-    </div>
-  );
-}
